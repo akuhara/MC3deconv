807fe2f2f61f9aed8dce1003432d509007063d95
Takeshi Akuhara <akuhara@eri.u-tokyo.ac.jp>
2018-10-29 12:46:16 +0900

    add temp.f90

---
+++ src/temp.f90
@@ -0,0 +1,21 @@
+subroutine setuptempladder(nchains, ncool, Tlow, Thigh, Chaintemp)
+  use mt19937
+  implicit none 
+#if defined MPI
+  include "mpif.h"
+#endif
+  integer, intent(in)  :: nchains, ncool
+  real(8), intent(out) :: chaintemp(nchains)
+  real(8), intent(in)  :: Tlow,Thigh
+  integer :: it
+  real(8) :: aval, bval
+  
+  aval = log(Tlow)
+  bval = log(Thigh)
+  do it = ncool + 1, nchains
+     chaintemp(it) = exp(aval + grnd()*(bval-aval))
+  end do
+  chaintemp(1:ncool) = Tlow                   ! Force first chain to be at Tlow
+  
+  return
+end subroutine setuptempladder
